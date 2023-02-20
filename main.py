77e9b112a851a15b35a60b99d1bf0b7a8fc6b67e
Bartek <bartex38@gmail.com>
2023-02-20 22:19:40 +0100

    do spanka

--- main.py
+++ main.py
@@ -1,52 +1,69 @@
-# def display_board(board):
-#
-#
-# #
-# # Funkcja, która przyjmuje jeden parametr zawierający bieżący stan tablicy
-# # i wyświetla go w oknie konsoli.
-# #
-#
-# def enter_move(board):
-#
-#
-# #
-# # Funkcja, która przyjmuje parametr odzwierciedlający biężący stan tablicy,
-# # prosi użytkownika o wykonanie ruchu,
-# # sprawdza dane wejściowe i aktualizuje tablicę zgodnie z decyzją użytkownika.
-# #
-#
-# def make_list_of_free_fields(board):
-#
-#
-# #
-# # Funkcja, która przegląda tablicę i tworzy listę wszystkich wolnych pól;
-# # lista składa się z krotek, a każda krotka zawiera parę liczb odzwierciedlających rząd i kolumnę.
-# #
-#
-# def victory_for(board, sign):
-#
-#
-# #
-# # Funkcja, która dokonuje analizy stanu tablicy w celu sprawdzenia
-# # czy użytkownik/gracz stosujący "O" lub "X" wygrał rozgrywkę.
-# #
+import random
 
-def draw_move(board):
-    board='''
+
+def wytwornia_list():
+    miejsce={}
+    for i in range (1,11):
+        miejsce[i] = " "
+
+    return miejsce
+
+
+def draw_move(lista):
+
+    board=f'''
     +-------+-------+-------+
     |       |       |       |
-    |   1   |   2   |   3   |
+    |   {pm(1,lista)}   |   {pm(2,lista)}   |   {pm(3,lista)}   |
     |       |       |       |
     +-------+-------+-------+
     |       |       |       |
-    |   4   |   X   |   6   |
+    |   {pm(4,lista)}   |   {pm(5,lista)}   |   {pm(6,lista)}   |
     |       |       |       |
     +-------+-------+-------+
     |       |       |       |
-    |   7   |   8   |   9   |
+    |   {pm(7,lista)}   |   {pm(8,lista)}   |   {pm(9,lista)}   |
     |       |       |       |
     +-------+-------+-------+
     '''
     return board
 
-print(draw_move("x"))
+def pm(i,lista): ## przypisywanie miejsca
+    return lista.get(i)
+
+def zmiana_na_planszy(lista,lokalizacja,znak):
+    lista[lokalizacja]=znak
+    return lista
+
+def ruch(kto):
+    wybor_gracza = int(input("blablabla"))
+    while wybor_gracza in pola_gracza or wybor_gracza in pola_komputera:
+        wybor_gracza = int(input("to miejsce jest zajete, wybierz inne:"))
+    pola_gracza.append(wybor_gracza)
+    for pozycyja in pola_gracza:
+        list = zmiana_na_planszy(lista, pozycyja, "o")
+    print(draw_move(list))
+
+lista = (wytwornia_list())
+lista[5]="x"
+pola_komputera=[5]
+pola_gracza=[]
+
+print(draw_move(lista))
+while True:
+
+    wybor_gracza = int(input("blablabla"))
+    while wybor_gracza in pola_gracza or wybor_gracza in pola_komputera:
+        wybor_gracza = int(input("to miejsce jest zajete, wybierz inne:"))
+    pola_gracza.append(wybor_gracza)
+    for pozycyja in pola_gracza:
+        list = zmiana_na_planszy(lista, pozycyja, "o")
+    print(draw_move(list))
+
+    wybor_komputera = random.randint(1,9)
+    while wybor_komputera in pola_gracza or wybor_komputera in pola_komputera:
+        wybor_komputera = random.randint(1,9)
+    pola_komputera.append(wybor_komputera)
+    for pozycja in pola_komputera:
+        lista = zmiana_na_planszy(lista,pozycja,"x")
+    print(draw_move(lista))
